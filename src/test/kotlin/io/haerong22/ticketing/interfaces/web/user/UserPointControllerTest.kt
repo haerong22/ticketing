ed16c907b3bd2b31fc0df2beb14f2775b118c0a8
haerong22 <haerong22@naver.com>
2024-04-08 18:49:37 +0900

    feat: 유저 포인트 충전 API

--- src/test/kotlin/io/haerong22/ticketing/interfaces/web/user/UserPointControllerTest.kt
+++ src/test/kotlin/io/haerong22/ticketing/interfaces/web/user/UserPointControllerTest.kt
@@ -1,17 +1,22 @@
 package io.haerong22.ticketing.interfaces.web.user
 
+import io.haerong22.ticketing.application.user.ChargeUserPointUseCase
 import io.haerong22.ticketing.application.user.GetUserPointUseCase
+import io.haerong22.ticketing.application.user.command.ChargeUserPointCommand
 import io.haerong22.ticketing.application.user.command.GetUserPointCommand
 import io.haerong22.ticketing.domain.user.User
 import io.haerong22.ticketing.domain.user.UserPoint
 import io.haerong22.ticketing.interfaces.web.WebTestSupport
+import io.haerong22.ticketing.interfaces.web.user.request.ChargeUserPointRequest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import org.mockito.BDDMockito.given
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -21,6 +26,9 @@ class UserPointControllerTest() : WebTestSupport() {
 
     @MockBean
     lateinit var getUserPointUseCase: GetUserPointUseCase
+
+    @MockBean
+    lateinit var chargeUserPointUseCase: ChargeUserPointUseCase
 
     @Test
     fun `유저 포인트 조회 성공`() {
@@ -52,5 +60,62 @@ class UserPointControllerTest() : WebTestSupport() {
             .andExpect(status().isBadRequest)
             .andExpect(jsonPath("$.code").value("400"))
             .andExpect(jsonPath("$.message").value("userId는 양수 값 입니다."))
+    }
+
+    @Test
+    fun `유저 포인트 충전 성공`() {
+        // given
+        val userId = 1L
+        val amount = 10000
+        val request = ChargeUserPointRequest(amount)
+
+        // then
+        mockMvc.perform(
+            patch("/api/users/%d/point".format(userId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+            .andDo(print())
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.code").value("0"))
+            .andExpect(jsonPath("$.message").value("success"))
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = [-1, 0])
+    fun `유저 포인트 충전 시 userId 는 양수이다`(userId: Long) {
+        // given
+        val amount = 10000
+        val request = ChargeUserPointRequest(amount)
+
+        // then
+        mockMvc.perform(
+            patch("/api/users/%d/point".format(userId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+            .andDo(print())
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.message").value("userId는 양수 값 입니다."))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0])
+    fun `유저 포인트 충전 시 amount 는 양수이다`(amount: Int) {
+        // given
+        val userId = 1L
+        val request = ChargeUserPointRequest(amount)
+
+        // then
+        mockMvc.perform(
+            patch("/api/users/%d/point".format(userId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
+        )
+            .andDo(print())
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.message").value("amount는 양수 값 입니다."))
     }
 }
