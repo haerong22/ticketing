0015dc9b0ef0895f92880878d706e6ed271e10ee
haerong22 <haerong22@naver.com>
2024-04-09 15:19:05 +0900

    chore: swagger 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,14 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // h2
     runtimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
