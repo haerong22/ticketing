c9d26ad81d0ceeccc8db41b30481f10fe1a7764c
haerong22 <haerong22@naver.com>
2024-04-10 18:39:46 +0900

    feat: Pageable 검증 추가

--- src/main/kotlin/io/haerong22/ticketing/domain/common/Pageable.kt
+++ src/main/kotlin/io/haerong22/ticketing/domain/common/Pageable.kt
@@ -1,7 +1,14 @@
 package io.haerong22.ticketing.domain.common
 
+import io.haerong22.ticketing.domain.common.CommonResponseCode.*
+
 class Pageable(
     val pageNo: Int,
     val pageSize: Int,
 ) {
+
+    init {
+        if (pageNo <= 0) throw BadRequestException(BAD_REQUEST, "pageNo는 1이상 값으로 초기화 할 수 있습니다.")
+        if (pageSize <= 0) throw BadRequestException(BAD_REQUEST, "pageSize는 1이상 값으로 초기화 할 수 있습니다.")
+    }
 }
--- src/test/kotlin/io/haerong22/ticketing/domain/common/PageableTest.kt
+++ src/test/kotlin/io/haerong22/ticketing/domain/common/PageableTest.kt
@@ -1,5 +1,32 @@
 package io.haerong22.ticketing.domain.common
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
-class PageableTest
+class PageableTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0])
+    fun `pageNo 음수 또는 0 으로 초기화 하면 BadRequestException 이 발생한다`(pageNo: Int) {
+        // given
+        val pageSize = 5
+
+        // when, then
+        assertThatThrownBy { Pageable(pageNo, pageSize) }
+            .isInstanceOf(BadRequestException::class.java)
+            .hasMessage("pageNo는 1이상 값으로 초기화 할 수 있습니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [-1, 0])
+    fun `pageSize 음수 또는 0 으로 초기화 하면 BadRequestException 이 발생한다`(pageSize: Int) {
+        // given
+        val pageNo = 1
+
+        // when, then
+        assertThatThrownBy { Pageable(pageNo, pageSize) }
+            .isInstanceOf(BadRequestException::class.java)
+            .hasMessage("pageSize는 1이상 값으로 초기화 할 수 있습니다.")
+    }
+}
