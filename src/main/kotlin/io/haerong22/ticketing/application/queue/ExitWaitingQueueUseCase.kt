e609f95fc401a25feb10d1a8f86d75702be440f7
haerong22 <haerong22@naver.com>
2024-04-17 19:06:30 +0900

    test: 대기열 퇴장 통합테스트

--- src/main/kotlin/io/haerong22/ticketing/application/queue/ExitWaitingQueueUseCase.kt
+++ src/main/kotlin/io/haerong22/ticketing/application/queue/ExitWaitingQueueUseCase.kt
@@ -2,7 +2,9 @@ package io.haerong22.ticketing.application.queue
 
 import io.haerong22.ticketing.domain.queue.QueueService
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @Service
 class ExitWaitingQueueUseCase(
     private val queueService: QueueService,
--- src/test/kotlin/io/haerong22/ticketing/application/queue/ExitWaitingQueueUseCaseTest.kt
+++ src/test/kotlin/io/haerong22/ticketing/application/queue/ExitWaitingQueueUseCaseTest.kt
@@ -1,5 +1,29 @@
 package io.haerong22.ticketing.application.queue
 
-import org.junit.jupiter.api.Assertions.*
+import io.haerong22.ticketing.application.IntegrationTestSupport
+import io.haerong22.ticketing.domain.common.enums.QueueStatus
+import io.haerong22.ticketing.infrastructure.db.queue.QueueEntity
+import io.haerong22.ticketing.infrastructure.db.queue.QueueJpaRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.*
 
-class ExitWaitingQueueUseCaseTest
+class ExitWaitingQueueUseCaseTest(
+    private val sut: ExitWaitingQueueUseCase,
+    private val queueJpaRepository: QueueJpaRepository,
+) : IntegrationTestSupport() {
+
+    @Test
+    fun `대기열 퇴장`() {
+        // given
+        val token = UUID.randomUUID().toString()
+        queueJpaRepository.save(QueueEntity(token = token, status = QueueStatus.WAITING))
+
+        // when
+        sut(token)
+
+        // then
+        val count = queueJpaRepository.count()
+        assertThat(count).isEqualTo(0)
+    }
+}
